bc6881a95b58e7c8ae1d3dd791882613754f83f0
OldFrostDragon <shevchukvb@gmail.com>
2016-02-14 13:32:21 +0200

    created plugin skeletion code

---
+++ JenkinsPlugin/jenkinspluginconstants.h
@@ -0,0 +1,13 @@
+#ifndef JENKINSPLUGINCONSTANTS_H
+#define JENKINSPLUGINCONSTANTS_H
+
+namespace JenkinsPlugin {
+namespace Constants {
+
+const char ACTION_ID[] = "JenkinsPlugin.Action";
+const char MENU_ID[] = "JenkinsPlugin.Menu";
+
+} // namespace JenkinsPlugin
+} // namespace Constants
+
+#endif // JENKINSPLUGINCONSTANTS_H
---
+++ JenkinsPlugin/jenkinspluginplugin.h
@@ -0,0 +1,31 @@
+#ifndef JENKINSPLUGIN_H
+#define JENKINSPLUGIN_H
+
+#include "jenkinsplugin_global.h"
+
+#include <extensionsystem/iplugin.h>
+
+namespace JenkinsPlugin {
+namespace Internal {
+
+class JenkinsPluginPlugin : public ExtensionSystem::IPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QtCreatorPlugin" FILE "JenkinsPlugin.json")
+
+public:
+    JenkinsPluginPlugin();
+    ~JenkinsPluginPlugin();
+
+    bool initialize(const QStringList &arguments, QString *errorString);
+    void extensionsInitialized();
+    ShutdownFlag aboutToShutdown();
+
+private slots:
+    void triggerAction();
+};
+
+} // namespace Internal
+} // namespace JenkinsPlugin
+
+#endif // JENKINSPLUGIN_H
